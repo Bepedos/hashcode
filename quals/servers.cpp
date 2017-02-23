0154d366a67261ee2c05189aac8de47ed152d413
Thomas Sibut-Pinote <thomas.sibut-pinote@inria.fr>
2017-02-23 20:59:18 +0100

    working score function

--- quals/servers.cpp
+++ quals/servers.cpp
@@ -131,6 +131,101 @@ struct SortByMagic {
 };
 
 
+int adjacency_e_c[MAX_E][MAX_C]; // adjacency matrix between endpoints and caches
+int adjacency_c_v[MAX_C][MAX_V]; // adjacency matrix between caches and videos
+
+void build_adjacency_matrix_c_v(Solution solution){
+  for(int c = 0;c<C;c++){
+    for(int v=0;v<V;v++){
+      adjacency_c_v[c][v] = 0;
+    }
+  };
+
+  for(int c=0;c<C;c++){
+    for(int i=0;i<solution.caches[c].size();i++){
+      adjacency_c_v[c][solution.caches[c][i]] = 1;
+    }
+  }
+}
+
+void build_adjacency_matrix_e_c(){
+  cerr << "building adjacency endpoint to cache matrix" << endl;
+  for(int e=0;e<E;e++){
+    for(int c=0;c<C;c++){
+      adjacency_e_c[e][c] = 0;
+    }
+  };
+
+  cerr << "there are" << E << " endpoints" << endl;
+  for(int e = 0;e<E;e++){
+    cerr << "size edges: " << edges[e].size() << endl;
+    for(int i=0;i<edges[e].size();i++){
+      // cerr << "considering edge" << i << " for endpoint " << e << endl;
+      Edge edge = edges[e][i]; // edge from e to c
+      int c = edge.c;
+      int l = edge.l;
+      if(l == 0){
+	cerr << "something went wrong, latency should not be 0!!!" << endl;
+      }
+      adjacency_e_c[e][c] = l;
+    }
+  }
+  cerr << "done building adjacency endpoint to cache matrix" << endl;
+}
+
+pair<double,double> compute_score(Solution solution){
+  build_adjacency_matrix_e_c();
+  // cerr << "printing endpoint to cache matrix" << endl;
+  // for(int e=0;e<E;e++){
+  //   for(int c=0;c<C;c++){
+  //     cerr << adjacency_e_c[e][c] << " ";
+  //   }
+  //   cerr << endl;
+  // }
+  
+  build_adjacency_matrix_c_v(solution);
+
+  // cerr << "printing cache to video matrix" << endl;
+  // for(int c=0;c<C;c++){
+  //   for(int v=0;v<V;v++){
+  //     cerr << adjacency_c_v[c][v] << " ";
+  //   }
+  //   cerr << endl;
+  // }
+
+  double score = 0;
+  int cumulated_requests = 0; // we will count the total number of requests
+  for(int i=0;i< requests.size();i++){
+    Request r = requests[i];
+    int video = r.v;
+    int endpoint = r.e;
+    // cerr << "treating request number " << i << " coming from endpoint " << endpoint << endl;
+    int number_requests = r.n;
+    cumulated_requests += r.n;
+    int datacenter_latency = latencies[endpoint];
+    // cerr << "  datacenter latency: " << datacenter_latency << endl;
+    int min_latency = datacenter_latency;
+    for(int cache=0;cache<C;cache++){
+      if(adjacency_e_c[endpoint][cache] != 0){
+	// cerr << endpoint << " and " << cache << " are connected" << endl;
+	if(adjacency_c_v[cache][video] ==1){
+	  int latency = adjacency_e_c[endpoint][cache];
+	  if(latency != 0){
+	    min_latency = min(min_latency,latency);
+	  }
+	}
+      }
+    }
+    // cerr << "  minimal latency: " << min_latency << endl;
+    // cerr << "  time saved per request: " << (datacenter_latency - min_latency) << endl;
+    // cerr << "  number of requests: " << number_requests << endl;
+    score += ((double) ((double) datacenter_latency - (double) min_latency)) * (double) number_requests;
+  }
+  double normalized_score = ((double) score) * (double) 1000 / (double) cumulated_requests;
+  return (make_pair(score,normalized_score));
+}
+
+
 Solution
 solve() {
   precompute_min_latencies();
@@ -168,6 +263,11 @@ main() {
   Solution sol = solve();
 
   sol.print();
+  
+  pair<double,double> score = compute_score(sol);
+  
+  cerr << "score:  unnormalized:" << score.first << " and normalized:  "  << score.second << endl;
+  
 
   return 0;
 }
