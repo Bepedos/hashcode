acb175c1529b49fe292c165303995d9fcfd339f4
Lucas Hosseini <lucas.hosseini@gmail.com>
2017-02-23 17:21:02 +0100

    C++ template

---
+++ quals/template.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+void
+read_input() {
+
+}
+
+void
+solve() {
+
+}
+
+int
+main() {
+  read_input();
+
+  solve();
+
+  return 0;
+}
