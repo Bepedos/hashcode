0b8c7a0c6cc88e4eff41910b6c4a4436babd65d9
Lucas Hosseini <lucas.hosseini@gmail.com>
2017-02-23 18:53:11 +0100

    C++ input.

---
+++ quals/servers.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <queue>
+#include <stack>
+
+using namespace std;
+
+const int MAX_E = 1000;
+const int MAX_V = 10000;
+
+struct Request {
+  int v, e, n;
+
+  Request(int v, int e, int n) : v(v), e(e), n(n) {}
+};
+
+int V, E, R, C, X;
+
+int videos[MAX_V];
+int latencies[MAX_E];
+int nb_caches[MAX_E];
+vector<pair<int, int> > edges[MAX_E];
+vector<Request> requests;
+
+void
+read_input() {
+  cin >> V >> E >> R >> C >> X;
+
+  for (int i = 0; i < V; ++i) {
+    cin >> videos[i];
+  }
+
+  for (int i = 0; i < E; ++i) {
+    cin >> latencies[i];
+    cin >> nb_caches[i];
+
+    for (int j = 0; j < nb_caches[i]; ++j) {
+      int c, l;
+      cin >> c >> l;
+      edges[i].push_back(make_pair(c, l));
+    }
+  }
+
+  for (int i = 0; i < R; ++i) {
+    int v, e, n;
+    cin >> v >> e >> n;
+    requests.push_back(Request(v, e, n));
+  }
+}
+
+void
+solve() {
+
+}
+
+int
+main() {
+  read_input();
+
+  solve();
+
+  return 0;
+}
